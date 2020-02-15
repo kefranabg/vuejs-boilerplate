77784a8390e0ad321c307d749c8e96e877b5cc62
Franck <abgrallkefran@gmail.com>
2020-02-15 20:35:06 +0100

    :wrench: Add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,27 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['airbnb-base', 'plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/essential',
+    'plugin:vue/base',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/require-default-prop': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
+    'no-var': 2,
+    'prefer-const': 2
   },
   overrides: [
     {
